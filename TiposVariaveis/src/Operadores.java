6049a9175be49314fe9df9ccc460909d63830bb3
Luis Felipe Gobetti <luis-sollu@hotmail.com>
2024-08-25 18:03:36 -0300

    25/08

---
+++ TiposVariaveis/src/Operadores.java
@@ -0,0 +1,42 @@
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		String nome = "José";
+		int idade = 22;
+		double peso = 22.5;
+		char sexo = 'M';
+		boolean doadorOrgao = false;
+//		Date dataNascimento =  new Date(); 
+		double soma = 10.5 + 15.0;
+		int subtracao = 13-5;
+		int mult = 1*5;
+		int div = 15/3;
+		int modulo = 18%3;
+		double resultado = (10*7) + (20/4);
+		
+		System.out.println(nome + " João");
+		
+		//Unários
+		int numero = 5;
+		
+		numero = -numero;
+		
+		System.out.println(numero);
+		System.out.println(numero);
+		
+		int numeroNovo = 5;
+		numeroNovo++; //incrementa ou decrementa um a partir de onde está
+		System.out.println(--numeroNovo);
+		
+		//Ternário
+		
+		int a = 6;
+		int b = 6;
+		
+		String resultadoT = a==b ? "verdadeiro" : "falso";
+		System.out.println(resultadoT);
+		//equals compara conteúdos
+	}
+
+}
--- TiposVariaveis/src/TiposVariaveis.java
+++ TiposVariaveis/src/TiposVariaveis.java
@@ -2,8 +2,14 @@
 public class TiposVariaveis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		double salarioMininimo = 2.500;
+		
+		short numeroCurto = 1;
+		int numeroNormal = numeroCurto;
+		short numeroCurto2 = (short)numeroNormal;
+		
+		final double PI = 3.14;
 	}
 
 }
+
